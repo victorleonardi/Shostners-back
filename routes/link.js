19aeca657859a501c3e5b6246d05c9ac8363752e
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2022-08-23 13:22:27 -0300

    Add view increment

--- routes/link.js
+++ routes/link.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const UrlModel = require('../models/urlModel')
+const UrlAccess = require('../models/urlAccess')
 const router = express.Router()
 const { getOne, checkDate } = require("../utils/middlewaresModel")
+const urlAccess = require('../models/urlAccess')
 
 // Get All
 
@@ -21,6 +23,23 @@ router.get('/:urlId', getOne, checkDate, async (req, res) => {
     console.log(res.onDate)
     if(res.onDate == true)    {
         const link = res.resultUrl.url
+        const encode = res.resultUrl.encode
+        const [ today ] = new Date().toISOString().split('T')
+
+        await UrlAccess.findOneAndUpdate(
+            {
+                $and:[
+                    {encode: encode},
+                    {created_at: {$gte: today}}
+                ]
+            },
+            {
+                $inc: {
+                    view: 1
+                }
+            }
+        )
+
         await res.redirect(link)
     }
 })
